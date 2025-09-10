ebd9bcf58f3a7b794f593c7be17b5dec60ca701c
Alka Tripathi <tripathialka382@gmail.com>
2025-09-11 00:32:14 +0530

    still on routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const userRouters = require("./routes/user");
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 8001;
@@ -11,6 +12,9 @@ app.set('views', path.resolve('./views'));
 app.get('/', (req, res) => {
   res.render('home.ejs');
 });
+
+// URL :- /user/signup ect
+app.use("/user",userRouters);
 app.listen(PORT, () => {
   console.log(`${PORT} Server is Listening..`);
 });
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,22 @@
-const express=require("express");
-const user=require("../models/users")
-const router=express.Router();
+const express = require('express');
+const user = require('../models/users');
+const router = express.Router();
 
-router.get("/signin",(req,res)=>{
-    return res.render("signin.ejs");
+router.get('/signin', (req, res) => {
+  return res.render('signin.ejs');
+});
 
-})
-router.post("/signup",async(req,res)=>{
-    const {fullname,email,password}=req.nody;
-    await user.create({
-        fullname,
-        email,
-        password,
-    })
-    return res.redirect("/");   //will redirect to the home [age]
-})
-module.exports=router;
+//post api
+router.get('/signup', (req, res) => {
+  return res.render('signup.ejs');
+});
+router.post('/signup', async (req, res) => {
+  const { fullname, email, password } = req.body;
+  await user.create({
+    fullname,
+    email,
+    password,
+  });
+  return res.redirect('/'); //will redirect to the home [age]
+});
+module.exports = router;
