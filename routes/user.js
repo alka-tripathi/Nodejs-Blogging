ff038a7fceee90f0f628725c22d2581558589268
Alka Tripathi <tripathialka382@gmail.com>
2025-09-10 23:57:31 +0530

    routing

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,18 @@
 const express=require("express");
+const user=require("../models/users")
 const router=express.Router();
 
 router.get("/signin",(req,res)=>{
     return res.render("signin.ejs");
 
+})
+router.post("/signup",async(req,res)=>{
+    const {fullname,email,password}=req.nody;
+    await user.create({
+        fullname,
+        email,
+        password,
+    })
+    return res.redirect("/");   //will redirect to the home [age]
 })
 module.exports=router;
